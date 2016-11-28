25f0923e39a9f6f3961f32ef79bf71e9eecf1951
Mr.zhong <214350457@qq.com>
2016-11-28 08:19:41 +0800

    上班了

--- app/src/main/java/com/rensisi/ariz/ariz/ui/ProceeFragment.java
+++ app/src/main/java/com/rensisi/ariz/ariz/ui/ProceeFragment.java
@@ -1,10 +1,12 @@
 package com.rensisi.ariz.ariz.ui;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
 
 import com.rensisi.ariz.ariz.Next;
 import com.rensisi.ariz.ariz.R;
@@ -22,6 +24,8 @@ public class ProceeFragment extends Next {
         switch (getFragmentType()) {
             case 5:
                 view = inflater.inflate(R.layout.fifth_fgm, container, false);
+                ScrollView scrollView = (ScrollView) view.findViewById(R.id.scv_fgm5);
+                new Handler().post(new ScrollRunner(scrollView));
                 break;
         }
         return view;
@@ -40,5 +44,17 @@ public class ProceeFragment extends Next {
     @Override
     public boolean save() {
         return false;
+    }
+
+    private static class ScrollRunner implements Runnable {
+        ScrollView scrollView;
+        public ScrollRunner(ScrollView scrollView) {
+            this.scrollView = scrollView;
+        }
+
+        @Override
+        public void run() {
+            scrollView.fullScroll(View.FOCUS_DOWN);
+        }
     }
 }
